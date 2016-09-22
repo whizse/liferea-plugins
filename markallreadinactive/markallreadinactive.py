49459ff5d08ee18e9ec36d1be56ce3cd01e9f251
Sven Arvidsson <sa@whiz.se>
2016-09-22 21:49:42 +0200

    Add license.

--- markallreadinactive/markallreadinactive.py
+++ markallreadinactive/markallreadinactive.py
@@ -1,3 +1,20 @@
+# Simple plugin to disable Mark All As Read
+#
+# Copyright 2016 Sven Arvidsson <sa@whiz.se>
+#
+# This plugin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from gi.repository import GObject
 from gi.repository import Liferea
 
