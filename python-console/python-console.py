21f49783ebba40b55e7c4856caf5aefb2527f944
Sven Arvidsson <sa@whiz.se>
2018-08-11 23:14:22 +0200

    python-console: Remove menuitem when plugin is unloaded.
    Add a helper function that removes a Gio.Action from A Gio.MenuModel.
    
    Add TODO.
    
    Print helpful message about loading code into console.

--- python-console/python-console.py
+++ python-console/python-console.py
@@ -1,7 +1,35 @@
+# TODO
+#  Handle primary selection, (middle mouse button paste)
+#  on first keypress, move input to right location
+#  more keyboard shortcuts:
+#      http://www.keyxl.com/aaaf192/83/Linux-Bash-Shell-keyboard-shortcuts.htm
+#      ctrl+r search history, tab for autocomplete etc..
+# Replace console with a sourceview?
+#      https://cfoch.github.io/tech/2017/08/24/tutorial-writing-a-plugin-in-pitivi-2017.html
+# Steal ideas from Rb console?
+#      https://github.com/GNOME/rhythmbox/blob/master/plugins/pythonconsole/pythonconsole.py
+# Don't reset the history when window is closed
+
 from gi.repository import GObject, Peas, Gtk, Gio
 from gi.repository import Liferea
 
 from pc import PythonConsole
+
+def remove_menuitem(action, menus, level=0):
+    """
+    Given an action name such as app.function, remove it from a Gio.Menu
+    """
+    for i in range(menus.get_n_items()):
+        link = menus.iterate_item_links(i)
+        if link.next():
+            remove_menuitem(action, link.get_value(), level+1)
+        else:            
+            attr = menus.iterate_item_attributes(i)
+            while attr.next():
+                if str(attr.get_name()) == "action":
+                    value = str(attr.get_value()).strip("'")
+                    if value == action:
+                        menus.remove(i)
 
 class PythonConsolePlugin(GObject.Object, Liferea.ShellActivatable):
     __gtype_name__ = 'PythonConsolePlugin'
@@ -14,7 +42,7 @@ class PythonConsolePlugin(GObject.Object, Liferea.ShellActivatable):
 
     def do_activate(self):
         action = Gio.SimpleAction.new('PythonConsole', None)
-        self.x = action.connect("activate", self.show_console, self.shell)
+        action.connect("activate", self.show_console, self.shell)
 
         self._app = self.shell.get_window().get_application ()
         self._app.add_action(action)
@@ -23,9 +51,8 @@ class PythonConsolePlugin(GObject.Object, Liferea.ShellActivatable):
         self.toolsmenu.append('Python Console', 'app.PythonConsole')
 
     def do_deactivate(self):
-        # FIXME: removes action but not the menu entry
-        # this doesn't seem possible in a Gio.Menu
         self._app.remove_action('PythonConsole')
+        remove_menuitem('app.PythonConsole', self.toolsmenu)
 
         if self.console_window is not None:
             self.console_window.destroy()
@@ -42,6 +69,8 @@ class PythonConsolePlugin(GObject.Object, Liferea.ShellActivatable):
                          _("You can access the main window " \
                          "through the \'shell\' variable :") +
                          '\\n%s" % shell)', False)
+            msg = "Run code using exec(open(\"/path/to/code.py\").read())"
+            console.eval('print(\'%s\')' % msg, False)
 
             self.console_window = Gtk.Window()
             self.console_window.set_title('Lifera Python Console')
